6350fe220394c0d7747d872eb280c08a0284da94
CKKH <chriskkhassan@gmail.com>
2018-12-28 18:40:40 +0000

    Run database migrations from the command line with Rake

--- config/datamapper.rb
+++ config/datamapper.rb
@@ -3,5 +3,3 @@ require './lib/message'
 DataMapper.setup(:default, "postgres://localhost/app_#{ENV["RACK_ENV"]}")
 
 DataMapper.finalize
-
-ENV["RACK_ENV"] == 'test' ? DataMapper.auto_migrate! : DataMapper.auto_upgrade!
