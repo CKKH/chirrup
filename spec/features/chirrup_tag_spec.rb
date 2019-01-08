a0a59efd479b927df6262e3d0134b63faa801c04
CKKH <chriskkhassan@gmail.com>
2019-01-08 16:04:56 +0000

    All tags displayed on index page

--- spec/features/chirrup_tag_spec.rb
+++ spec/features/chirrup_tag_spec.rb
@@ -3,19 +3,29 @@ require_relative 'web_helpers'
 
 feature 'Tags' do
   context 'Creating' do
-    scenario 'A user can submit text and see it with a timestamp' do
+    scenario 'A user can submit tag' do
       visit '/'
       expect(page).to have_field('tag')
     end
 
-    scenario 'A user can submit text and see it with a timestamp' do
+    scenario 'A user can submit tag' do
       visit '/'
-      expect(page).to have_field('tag')
       fill_in :tag, with: ('tag test')
       click_button 'Submit'
       expect(page).to have_text('tag test')
     end
   end
+
+    scenario 'A user can submit many tags and view them all' do
+      visit '/'
+      fill_in :tag, with: ('tag test')
+      click_button 'Submit'
+      visit '/'
+      fill_in :tag, with: ('tag test 2')
+      click_button 'Submit'
+      expect(page).to have_text('tag test')
+      expect(page).to have_text('tag test 2')
+    end
 
 end
 
